3864c25c3130f0bde643b800c4998d22f4a63295
refractored <me@refractored.net>
2024-09-16 19:07:23 -0500

    feat(Bloodmoon): Move bloodmoon to jitpack instead of jar

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,21 +9,23 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-
+    maven("https://jitpack.io")
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.auxilor.io/repository/maven-public/")
-    maven("https://jitpack.io")
     maven("https://oss.sonatype.org/content/groups/public/") {
         name = "sonatype"
     }
 }
 
 dependencies {
-    implementation(fileTree("libs"))
-    compileOnly("com.willfp:eco:6.55.0")
+    compileOnly(fileTree("libs"))
+    compileOnly("com.github.refractored:BloodmoonReloaded:main-SNAPSHOT")
+    compileOnly("com.github.Revxrsal.Lamp:common:3.2.1")
+    compileOnly("com.github.Revxrsal.Lamp:bukkit:3.2.1")
+    compileOnly("com.willfp:eco:6.74.1")
     compileOnly("com.github.Auxilor:libreforge:4.71.5")
     compileOnly("io.papermc.paper:paper-api:1.21.1-R0.1-SNAPSHOT")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:2.0.0")
+    compileOnly("org.jetbrains.kotlin:kotlin-stdlib:2.0.0")
 }
 
 val targetJavaVersion = 21
